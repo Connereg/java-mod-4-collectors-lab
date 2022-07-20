239d099cd2e858be7bb1e5ab5e6fce6dd4e7e330
Conner Gleason <connereg@optonline.net>
2022-07-20 13:39:27 -0400

    Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Main {
 
     // modify this method
     public static long multiplySquaresOfNumbers(List<Integer> numbers) {
         // your code here
-        return 0;
+        return numbers.stream().map(num -> num*num).collect(Collectors.reducing(1, Math::multiplyExact)); // IDENTITY TELLS IT TO MULTIPLY AGAINST ONE, SO WE DONT UNINTENTIONALLY AFFECT OUTCOME OF MULTIPLYING ALL NUMBERS IN STREAM
+        // REDUCE IS AN AGGRAGATE METHOD, MUST START WITH A NUMBER TO AFFECT
     }
 
     public static void main(String[] args) {
